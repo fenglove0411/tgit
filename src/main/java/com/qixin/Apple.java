eb2ac77de8fcbe2b7aa3f7743c0546b682344b7e
eclipse_user <test_glb@feng.com>
2018-11-19 22:38:47 +0800

    热修复NO2

--- src/main/java/com/qixin/Apple.java
+++ src/main/java/com/qixin/Apple.java
@@ -3,6 +3,10 @@ package com.qixin;
 public class Apple {
 	public void apple() {
 		System.out.println("热修复hot_fix");
+		System.out.println("热修复hot_fix");
+		System.out.println("热修复hot_fix");
+		System.out.println("热修复hot_fix");
+		System.out.println("热修复hot_fix");
 	}
 
 }
